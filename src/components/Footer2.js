a335ed6469517fc9f2c6ace7b6e998bc5e32e431
Abhi06027 <abhinavk67@gmail.com>
2021-12-21 16:01:56 +0530

    shop

---
+++ src/components/Footer2.js
@@ -0,0 +1,58 @@
+const Footer2  = () => {
+    return ( 
+        <div className="   "  > 
+   <div className=" lg:flex lg:justify-center gap-32 mt-16 px-7 lg:px-0  " >
+      
+      <div     >
+       <div className=" font-bold text-4xl mb-5 cursor-pointer  "   >   Estore.     </div> 
+       <div className=" hidden lg:block  ">   Lorem ipsum dolor sit amet, <br></br> consectetur adipisicing elit sed <br></br> do eiusmod tempor incididunt <br></br> ut labore.    </div> 
+       <div className=" lg:hidden  ">   Lorem ipsum dolor sit amet,  consectetur adipisicing elit sed  do eiusmod tempor incididunt  ut labore.    </div> 
+     </div>
+<div>
+
+        <div className=" mb-5 font-bold mt-5 lg:mt-0  "   >   Quick Links    </div>   
+        <div className=" mb-2 hover:text-blue-700 "   >   About   </div>   
+        <div className=" mb-2 hover:text-blue-700   "   >   Offers & Discounts   </div>   
+        <div className=" mb-2 hover:text-blue-700  "   >   Get Coupon    </div>   
+        <div className=" mb-2 hover:text-blue-700   "   >   Contact Us    </div>   
+        </div>
+
+
+<div>
+<div className=" mb-5 font-bold mt-5 lg:mt-0   "   >  New Products   </div> 
+        <div className="  mb-2 hover:text-blue-700  "   >   Woman Cloth  </div>     
+        <div className=" mb-2  hover:text-blue-700  "   >   Fashion Accessories  </div>  
+        <div className="  mb-2 hover:text-blue-700  "   >  Man Accessories   </div>  
+        <div className=" mb-2  hover:text-blue-700  "   >  Rubber made Toys  </div> 
+       
+       </div>      
+         <div>
+         
+         <div className=" mb-5 font-bold  mt-5 lg:mt-0  "   >  Support   </div> 
+         <div className=" mb-2 hover:text-blue-700  "   >  Frequently Asked Questions  </div> 
+         <div className=" mb-2 hover:text-blue-700 "   > Terms & Conditions  </div> 
+         <div className=" mb-2 hover:text-blue-700 "   >  Privacy Policy  </div> 
+         <div className=" mb-2 hover:text-blue-700 "   >  Report a Payment Issue   </div> 
+         </div>
+         </div>  
+      
+        </div>
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+     );
+}
+ 
+export default Footer2 ;
